bed98f5ecc23b26357018e2beefb0f17120878a4
Muhammad Mahad <mahadtxt@gmail.com>
2024-11-16 19:39:13 +0500

    alternate_upper_lower_case: more more info

--- miscellaneous/alternate_upper_lower_case.cpp
+++ miscellaneous/alternate_upper_lower_case.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 // alternate between upper and lower case
 // XOR with 32
+// learn more here: https://codeforces.com/blog/entry/99055
 int main() {
   while (true) {
     string S;
     cin >> S;
     for (auto &c : S) {
       c ^= 32;
+      // c ^= ' ';
       cout << c;
     }
     cout << '\n';
